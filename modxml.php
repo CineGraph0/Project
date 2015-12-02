e17a63a65936d7567fd727122ba403f5624f23a4
x14503387 <markmcd1993@gmail.com>
2015-12-02 13:53:41 +0000

    test on php

--- modxml.php
+++ modxml.php
@@ -1,26 +1,30 @@
 <?php
-
 /*
- * PHP SimpleXML
  * Loading a XML from a file, adding new elements and editing elements
  */
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 //get author from form
 $type = $_POST["type"];
 $name = $_POST["name"];
 $ram = $_POST["ram"];
 $price = $_POST["type"];
-
+echo $type;
 
 if (file_exists('product.xml')) {
     //loads the xml and returns a simplexml object
     $xml = simplexml_load_file('product.xml');
-
+    //print_r($xml);
+    
     //transforming the object in xml format
     $xmlFormat = $xml->asXML();
+    //echo htmlentities($xmlFormat);
+
+
     //displaying the element in proper format
-    echo '<u><b>This is the xml code from test2.xml:</b></u>
+  echo '<u><b>This is the xml code from test2.xml:</b></u>
      <br /><br />
-     <pre>' . htmlentities($xmlFormat, ENT_COMPAT | ENT_HTML401, "ISO-8859-1") . '</pre><br /><br />';
+     <pre>' . htmlentities($xmlFormat, ENT_COMPAT | ENT_HTML401, "ISO-8859-1") . '</pre><br /><br />'; 
 
     //adding new child to the xml
     $newChild = $xml->addChild('secondhand');
@@ -31,25 +35,17 @@ if (file_exists('product.xml')) {
   
     //transforming the object in xml format
     $xmlFormat = $xml->asXML();
+   // echo htmlentities($xmlFormat);
+
     //displaying the element in proper format
-    echo '<u><b>This is the xml code from product.xml with new elements added:</b></u>
+   echo '<u><b>This is the xml code from product.xml with new elements added:</b></u>
      <br /><br />
      <pre>' . htmlentities($xmlFormat, ENT_COMPAT | ENT_HTML401, "ISO-8859-1") . '</pre>';
 
-    //changing the nodes values
-    //in this case we are changing the value 
-    //of all children called <name>
-    foreach ($xml->children() as $child)
-        $child->type = "CHANGED";
-        $child->name = "CHANGED";
-        $child->ram = "CHANGED";
-        $child->price = "CHANGED";
-    //displaying the element in proper format
-   echo '<br /><u><b>This is the xml code from books.xml with all genre changed:</b></u>
-    <br /><br />
-    <pre>' . htmlentities($xml->asXML(), ENT_COMPAT | ENT_HTML401, "ISO-8859-1") . '</pre>';
+file_put_contents('product.xml', $xml->saveXML());
+
 } else {
-    exit('Failed to open product.xml.');
+    exit('Failed to open product.xml.'); 
 }
-    file_put_contents('/ide.c9.io/x14503387/project/product.xml', $xml->asXML());
+    
 ?>
